b14637bda199911054b62d1f178b5b80fc7afc8f
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-08-28 00:00:44 -0700

    Create Artist and Release models
    - Add Artist and Release models in models.py with initial fields
    - Register Artist and Release models in admin.py
    - Run makemigrations and migrate
    - Create 'admin' superuser
    - Update notes.md

--- src/releases/admin.py
+++ src/releases/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Artist, Release
 
-# Register your models here.
+admin.site.register(Artist)
+admin.site.register(Release)
--- src/releases/models.py
+++ src/releases/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+
+class Artist(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    bio = models.TextField(blank=True)
+
+
+class Release(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    about = models.TextField(blank=True)
