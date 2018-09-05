e765fab652f921450d2e53bd698bb31e0b5b5a25
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-09-05 15:39:57 -0700

    Add more fields and todos to models.py; update notes.md

--- src/releases/admin.py
+++ src/releases/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
 from .models import Artist, Release
 
-admin.site.register(Artist)
-admin.site.register(Release)
+admin.site.register((Artist, Release))
--- src/releases/models.py
+++ src/releases/models.py
@@ -6,6 +6,8 @@ class Artist(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=100)
     bio = models.TextField(blank=True)
+    genre = models.CharField(max_length=100)
+    # TODO: Image link
 
 
 class Release(models.Model):
@@ -13,3 +15,7 @@ class Release(models.Model):
     title = models.CharField(max_length=200)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     about = models.TextField(blank=True)
+    year = models.IntegerField(blank=True)
+    # TODO: Streaming link
+    # TODO: Artwork link
+    # TODO: Tracklist
