b6a533cc166cf39ab680598643af0bf31b0e633d
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-09-14 22:41:23 -0700

    Add artwork and url fields to Release model
    modified `releases/models.py`:
      - Add artwork and streaming_url field to Release model
      - Add define __str__ for Artist and Release model

--- src/releases/models.py
+++ src/releases/models.py
@@ -9,6 +9,9 @@ class Artist(models.Model):
     genre = models.CharField(max_length=100, blank=True)
     image = models.ImageField(null=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Release(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
@@ -16,6 +19,9 @@ class Release(models.Model):
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     about = models.TextField(blank=True)
     year = models.IntegerField(blank=True, null=True)
-    # TODO: Streaming link
-    # TODO: Artwork
-    # TODO: Tracklist
+    artwork = models.ImageField(null=True, blank=True)
+    streaming_url = models.URLField(null=True, blank=True)
+    # TODO: Tracklist?
+
+    def __str__(self):
+        return f'{self.artist} - {self.title}'
