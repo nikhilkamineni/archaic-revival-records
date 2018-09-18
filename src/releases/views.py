9650312cb3525daf76307d4179bff569518ad8b6
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-09-17 21:31:33 -0700

    Add basic views and paths
    modified:   src/arr/urls.py
      - Add path to `releases.urls` to root endpoint
    new file:   src/releases/urls.py
      - Add paths to "", "artists/" and "releases/" in
        releases.urls
    modified:   src/releases/views.py
      - Add `index`, `artists` and `releases` views

--- src/releases/views.py
+++ src/releases/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Welcome to Archaic Revival Records")
+
+
+def artists(request):
+    return HttpResponse("You're at the Artists page")
+
+
+def releases(request):
+    return HttpResponse("You're at the Releases page")
